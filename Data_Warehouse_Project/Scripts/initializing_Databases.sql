9073ad1a576732eb1d08fe2b259298de16bf9d19
Sai Divya Kilari <168947015+SaiDivyakilari@users.noreply.github.com>
2026-02-23 12:08:31 -0700

    Enhance comments in initializing Databases script
    Updated script to include detailed comments on database creation and warnings about data loss.

--- Data_Warehouse_Project/Scripts/initializing_Databases.sql
+++ Data_Warehouse_Project/Scripts/initializing_Databases.sql
@@ -1,11 +1,24 @@
-/* Creating Databases while checking if they are exists or not */
 
+/*
+============================================
+Create DataBase
+============================================
+Script purpose :
+			This script create a Database named Data_Warehouse. Initially we drop the database if it already exists and then create
+            a new database. We also create three databases 'Bronze','Silver','Gold' while checking if these databases exists or not
+Warning :
+		Running this script will delete the entire database if exists, all the data inside teh databse will get deleted
+*/
+            
+
+DROP DATABASE IF EXISTS Data_Warehouse;
 Create DATABASE Data_Warehouse;
 USE Data_Warehouse;
 
 -- In MySQL Schema=Database (wheras in SQL Server, inside the database we create Schemas which holds the tables of their own)
 /*A schema is a logical container within a database used to organize objects such as tables, views, and procedures. 
 It helps manage structure, security, and naming within a database.*/
+
 
 CREATE SCHEMA Bronze;
 CREATE DATABASE Silver;
