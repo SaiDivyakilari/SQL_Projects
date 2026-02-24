da2bfe0177c59d6028cae4924459b446ba851b82
Sai Divya Kilari <168947015+SaiDivyakilari@users.noreply.github.com>
2026-02-24 12:53:18 -0700

    Add comments to DDL_bronze.sql script
    Added header comments to DDL script for bronze tables.

--- Data_Warehouse_Project/Scripts/Bronze/DDL_bronze.sql
+++ Data_Warehouse_Project/Scripts/Bronze/DDL_bronze.sql
@@ -1,3 +1,14 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' database, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
 USE Bronze;
 DROP TABLE IF EXISTS crm_cust_info;
 CREATE TABLE crm_cust_info(
